be50de8eb40dcedec2493686091ea2895cd8c00d
Kellen <bolger.kellen@gmail.com>
2024-10-23 18:21:51 -0400

    chore: remove old files

--- src/utils/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (date: Date | string) => {
-  const newDate = new Date(date);
-  return newDate.toLocaleString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    timeZone: 'UTC',
-  });
-};
