7a85931437b7b4e605d3c0763ff2ebb08079e4a5
bolg55 <kellen.bolger@strapi.io>
2024-10-24 12:55:12 -0400

    chore: config strapi fetch

---
+++ frontend/src/queries/strapiQueries.ts
@@ -0,0 +1,21 @@
+import { getStrapiURL } from '@/utils/strapi';
+
+export const fetchStrapiData = async (
+  endpoint: string,
+  token: string | null = null
+) => {
+  try {
+    const headers: Record<string, string> = {};
+    const res = await fetch(`${getStrapiURL()}/api${endpoint}`, { headers });
+
+    if (!res.ok) {
+      throw new Error('An error occurred while fetching the data');
+    }
+
+    const { data } = await res.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
