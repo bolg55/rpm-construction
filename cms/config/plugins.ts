b970b8539eabd9d9a82bc0a72b7970bfd0c2ea4c
Kellen <bolger.kellen@gmail.com>
2024-10-27 22:07:41 -0400

    fix: update configs

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,14 +1,1 @@
-export default ({ env }) => ({
-  upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
-    },
-    actionOptions: {
-      upload: {},
-      delete: {},
-    },
-  },
-});
+export default () => ({});
