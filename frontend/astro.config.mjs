0d70ca6ebad5ca4de822a10e07c49fb1f8ebe86b
Kellen <bolger.kellen@gmail.com>
2024-11-09 17:59:02 -0500

    fix: change config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -11,7 +11,7 @@ import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.CF_PAGES_URL,
+  site: process.env.CF_PAGES_URL,
   integrations: [react(), tailwind(), sitemap(), robotsTxt(), icon(), mdx()],
   markdown: {
     remarkPlugins: [remarkGfm],
