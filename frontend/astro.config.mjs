00d8da86507e0a29b6cb4add24b286a550293530
Kellen <bolger.kellen@gmail.com>
2024-12-30 13:54:30 -0500

    fix: add site url

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -11,7 +11,7 @@ import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CF_PAGES_URL,
+  site: 'https://rpmconstructionltd.com',
   integrations: [react(), tailwind(), sitemap(), robotsTxt(), icon(), mdx()],
   markdown: {
     remarkPlugins: [remarkGfm],
